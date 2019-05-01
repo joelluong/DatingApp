03004f77f3318125950fb55cfe8c8f754f780fd5
joelluong <luongdaitrungduong@gmail.com>
2019-05-01 14:27:21 +1000

    ReactiveFormInAngular

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -32,6 +32,7 @@ export class PhotoEditorComponent implements OnInit {
   }
 
   initializeUploader() {
+    // upload photo to api
     this.uploader = new FileUploader({
       url: this.baseUrl + 'users/' + this.authService.decodedToken.nameid + '/photos',
       authToken: 'Bearer ' + localStorage.getItem('token'),
@@ -55,6 +56,12 @@ export class PhotoEditorComponent implements OnInit {
           isMain: res.isMain
         };
         this.photos.push(photo);
+
+        if (photo.isMain) {
+          this.authService.changeMemberPhoto(photo.url);
+          this.authService.currentUser.photoUrl = photo.url;
+          localStorage.setItem('user', JSON.stringify(this.authService.currentUser)); // set local storage
+        }
       }
     };
   }
